314a2245681696a52102a3b008b35f668af9b9c4
matias0621 <mattiascorrea@outlook.com>
2025-07-30 21:27:54 -0300

    double spiral parttern

--- Script/bullet_controller.gd
+++ Script/bullet_controller.gd
@@ -1,15 +1,19 @@
 extends Node
 
 var attack = true
-@export var amount_bullet:int = 5
+@export var amount_bullet:int = 10
 var rotate = 0
 const BULLET = preload("res://Scene/bullet.tscn")
 var time = 0.0
-@export var time_wait = 1.0
+@export var time_wait = 0.1
+@export var test = 0
 
 
 func _process(delta: float) -> void:
-	circle_pattern(delta)
+	if test == 0:
+		double_spiral_pattern(delta)
+	elif test == 1:
+		spiral_pattern(delta)
 
 func circle_pattern(delta:float) -> void:
 	time += delta
@@ -31,10 +35,21 @@ func circle_pattern(delta:float) -> void:
 func spiral_pattern(delta:float) -> void:
 	time += delta
 	if time > time_wait:
-		rotate -= get_rotation_for_bullet()
+		rotate += get_rotation_for_bullet()
 		create_bullet()
 		time = 0.0
 
+func double_spiral_pattern(delta:float) -> void:
+	time += delta
+	var save_rotation:float
+	if time > time_wait:
+		rotate += get_rotation_for_bullet()
+		save_rotation = rotate
+		create_bullet()
+		rotate = 180 + save_rotation
+		create_bullet()
+		rotate = save_rotation - 180
+		time = 0
 
 func create_bullet() -> void:
 	var bullet: Bullet = BULLET.instantiate()
